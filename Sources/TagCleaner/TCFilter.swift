965692e580e4466b19629a20d9a75de08b542e9d
Tomás Martins <tomas.marti@me.com>
2023-12-05 19:02:30 -0300

    Added TCFilter enum

---
+++ Sources/TagCleaner/TCFilter.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tomas Martins on 05/12/23.
+//
+
+import Foundation
+
+enum TCFilter {
+    case cleanExplicit
+    case featuredArtists
+    case remastered
+    case reissue
+}
