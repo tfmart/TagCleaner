0acd21f6d4397ef05e0eaa52b16aec679a1be0b2
Tomás Martins <tomas.marti@me.com>
2023-12-05 13:13:18 -0300

    Added initial rules (explicit, clean, feature, live)

--- Sources/TagCleaner/TagCleaner.swift
+++ Sources/TagCleaner/TagCleaner.swift
@@ -1,2 +1,97 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  TagCleaner.swift
+//
+//
+//  Created by Tomas Martins on 04/12/23.
+//
+
+import Foundation
+import RegexBuilder
+
+public struct TCFilterRule {
+    /// String or pattern to replace.
+    public let source: Regex<Substring>
+    /// Replacement string.
+    public let target: String
+}
+
+public struct TCFilter {
+    public static var cleanExplicitFilterRules: [TCFilterRule] {
+        let regex = Regex {
+            One(.whitespace)
+            ChoiceOf {
+                Regex {
+                    "["
+                    ChoiceOf {
+                        "Explicit"
+                        "Clean"
+                    }
+                    "]"
+                }
+                Regex {
+                    "("
+                    ChoiceOf {
+                        "Explicit"
+                        "Clean"
+                    }
+                    ")"
+                }
+            }
+        }
+        return [TCFilterRule(source: regex,
+                             target: "")]
+    }
+    
+    public static var featureFilterRules: [TCFilterRule] {
+        let regex = Regex {
+            ChoiceOf {
+                Regex {
+                    ZeroOrMore(.whitespace)
+                    "(feat."
+                    ZeroOrMore(CharacterClass(.anyOf(")"), .anyOf(")")).inverted)
+                    ")"
+                }
+                Regex {
+                    ZeroOrMore(.whitespace)
+                    "[feat."
+                    ZeroOrMore(CharacterClass(.anyOf("]"), .anyOf("]")).inverted)
+                    "]"
+                }
+                Regex {
+                    ZeroOrMore(.whitespace)
+                    "feat."
+                    ZeroOrMore(.any)
+                }
+            }
+        }
+        
+        return [TCFilterRule(source: regex,
+                             target: "")]
+    }
+    
+    public static var liveFilterRules: [TCFilterRule] {
+        let regex = Regex {
+            OneOrMore(.whitespace)
+            "-"
+            OneOrMore(.whitespace)
+            ChoiceOf {
+                Regex {
+                    ChoiceOf {
+                        "Live"
+                        Regex {
+                            "Live at"
+                            ZeroOrMore(.any)
+                        }
+                    }
+                }
+                
+                Regex {
+                    "(Live)"
+                }
+            }
+        }
+        
+        return [TCFilterRule(source: regex,
+                             target: "")]
+    }
+}
--- Tests/TagCleanerTests/TagCleanerTests.swift
+++ Tests/TagCleanerTests/TagCleanerTests.swift
@@ -1,12 +1,51 @@
 import XCTest
+import RegexBuilder
 @testable import TagCleaner
 
 final class TagCleanerTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testCleanExplicitRule() {
+        let explicitInput = "One More Night (Explicit)"
+        
+        var filteredOutput = explicitInput
+        let rules = TCFilter.cleanExplicitFilterRules
+        for rule in rules {
+            filteredOutput = filteredOutput.replacing(rule.source, with: "")
+        }
+        
+        XCTAssertNotEqual(explicitInput, filteredOutput)
+        XCTAssertEqual(filteredOutput, "One More Night")
+    }
+    
+    func testCleanRule() {
+        let cleanInput = "One More Night (Clean)"
+        var filteredOutput = cleanInput
+        let rules = TCFilter.cleanExplicitFilterRules
+        for rule in rules {
+            filteredOutput = filteredOutput.replacing(rule.source, with: "")
+        }
+        
+        XCTAssertNotEqual(cleanInput, filteredOutput)
+        XCTAssertEqual(filteredOutput, "One More Night")
+    }
+    
+    func testFeatRule() {
+        let inputWithParentheses = "After the Storm (feat. Tyler, The Creator & Bootsy Collins)"
+        let inputWithBrackets = "After the Storm [feat. Tyler, The Creator & Bootsy Collins]"
+        let inputWithouSeparator = "After the Storm feat. Tyler, The Creator & Bootsy Collins"
+        
+        var output1: String = inputWithParentheses
+        var output2: String = inputWithBrackets
+        var output3: String = inputWithouSeparator
+        
+        let rules = TCFilter.featureFilterRules
+        for rule in rules {
+            output1 = output1.replacing(rule.source, with: "")
+            output2 = output2.replacing(rule.source, with: "")
+            output3 = output3.replacing(rule.source, with: "")
+        }
+        
+        XCTAssertEqual(output1, "After the Storm")
+        XCTAssertEqual(output2, "After the Storm")
+        XCTAssertEqual(output3, "After the Storm")
     }
 }
